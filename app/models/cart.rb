861c67b1ad62ac3f126d1de8fee9323870f1a629
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-09-05 01:29:22 -0300

    Feat: Models Cart, CartItem & product

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,19 @@
 class Cart < ApplicationRecord
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
+  has_many :cart_items, depentent: :destroy
+  has_many :products, through: :cart_items
+
+  enum status: { active: 0, abandoned: 1}
+
+  #Calcula o preço total do carrinho de acordo
+  def total_price
+    cart_items.sum(&:total_price)
+  end
+
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  def update_last_interaction!
+    update!(update_at: Time.current)
+  end
 
 end
---
+++ app/models/cart_item.rb
@@ -0,0 +1,14 @@
+class CartItem < AplicationRecord
+  belongs_to :cart
+  belongs_to :product
+
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+
+  def total_price
+    quantity * unit_price
+  end
+
+  def unit_price
+    product.price
+  end
+end
