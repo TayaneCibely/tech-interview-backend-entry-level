9e2d20fc4c2909fd9bf3bab043ff0eeb440651fb
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-09-06 02:46:18 -0300

    Fix: erros de digitação corrigidos nos models

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,4 @@
-class CartItem < AplicationRecord
+class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resource :cart, only: [:show, :create] do
     post :add_item
-    delete 'rproduct_id', to: 'carts#remove_item', as: 'remove_item'
+    delete ':product_id', to: 'carts#remove_item', as: :remove_item
   end
 end
