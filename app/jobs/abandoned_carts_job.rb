1527e906107c246fbb09e98514ed0f74bf351937
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-09-06 00:53:39 -0300

    Feat: Job que considerar o carrinho abandonado após 3 horas sem interação e remove depois de 7 dias

--- app/jobs/abandoned_carts_job.rb
+++ app/jobs/abandoned_carts_job.rb
@@ -1,4 +1,22 @@
-# frozen_string_literal: true
+class AbandonedCartsJob < ApplicationJob
+  queue_as :default
 
-class AbandonedCartsJob
+  def perform
+    mark_as_abandoned
+    remove_old_abandoned_carts
+  end
+
+  private
+
+  def mark_as_abandoned
+    Cart.active
+        .where('updated_at < ?', 3.hours.ago)
+        .update_all(status: :abandoned, updated_at: Time.current)
+  end
+
+  def remove_old_abandoned_carts
+    Cart.abandoned
+        .where('updated_at < ?', 7.days.ago)
+        .destroy_all
+  end
 end
