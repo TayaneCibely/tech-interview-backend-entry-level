ba06c169c304cedb8537231303cf0d6a34b6a511
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-09-06 00:50:19 -0300

    Feat: Services com a lógica de negórcio para remover o item e base que define o comportamento dos demais services

--- app/services/cart_services/base.rb
+++ app/services/cart_services/base.rb
@@ -1,4 +1,32 @@
-# frozen_string_literal: true
+module CartServices
+  class Base
+    attr_reader :success, :errors, :error, :status
 
-module Base
+    def initialize
+      @success = false
+      @errors = []
+      @error = nil
+      @status = :unprocessable_entity
+    end
+
+    def self.call(*args)
+      new.call(*args)
+    end
+
+    def success?
+      @success
+    end
+
+    private
+
+    def success!
+      @success = true
+    end
+
+    def add_error(message, status = :unprocessable_entity)
+      @errors << message
+      @error = message
+      @status = status
+    end
+  end
 end
--- app/services/cart_services/remove_item.rb
+++ app/services/cart_services/remove_item.rb
@@ -1,4 +1,18 @@
-# frozen_string_literal: true
 
-module RemoveItem
+module CartService
+  class RemoveItem < Base
+    def call(cart, product_id)
+      cart_item = cart.cart_items.find_by(product_id: product_id)
+
+      unless product
+        add_error("Product not found", :not_found)
+        return self
+      end
+
+      cart_item.destroy!
+      cart.update_last_interaction!
+      success!
+      self
+    end
+  end
 end
