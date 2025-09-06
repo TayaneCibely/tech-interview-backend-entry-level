3233a43dfcae3bc420d2a7e75ef68aa1a4b7577f
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-09-06 00:51:34 -0300

    Config: Definição das rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
-require 'sidekiq/web'
-
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq'
-  resources :products
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  root "rails/health#show"
+  resource :cart, only: [:show, :create] do
+    post :add_item
+    delete 'rproduct_id', to: 'carts#remove_item', as: 'remove_item'
+  end
 end
